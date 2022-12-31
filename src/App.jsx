de78d0769e93f13769e408a949d0629a6fd98276
Abdullah-Ahmed1 <abdullah.ahmed10001@gmail.com>
2023-01-01 00:03:02 +0500

    first action dispatched

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import store from './store'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  store.dispatch({
+      type:"bugAdded",
+      payload  :{
+        description: "bug1"
+      }
+  })
+  console.log(store.getState())
   return (
     <div className="App">
         hello world
